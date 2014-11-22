00b05db25d8bbbc972a2c9ae0bc7d351dfd2f746
root <root@learning.puppetlabs.vm>
2014-11-22 12:29:53 +0000

    initial commit- taking backup

---
+++ modules/lvmguide/manifests/init.pp
@@ -0,0 +1,27 @@
+class lvmguide (
+  $document_root = '/var/www/html/lvmguide',
+  $port          = '80',
+) {
+  #####33333333333333333333
+  #service {'apache2':
+  #  ensure=> running,
+  #}
+  file {'/var/www/html':
+    ensure => directory,
+  }
+  file {'/var/www/html/lvmguide':
+    ensure  =>directory,
+    source  =>"puppet:///modules/lvmguide/lvmguide",
+    recurse =>true,
+  }
+  ##########################
+  # Manage apache, the files for the website will be 
+  # managed by the quest tool
+  class { 'apache': 
+    default_vhost => false,
+  }
+  apache::vhost { 'learning.puppetlabs.vm':
+    port    => $port,
+    docroot => $document_root,
+  }
+}
