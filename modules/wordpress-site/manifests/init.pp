6b2f72acab7ca69c2e9675db5cc02c9fa7dd7ebe
bingoarunprasath <arunprasath33@gmail.com>
2014-11-30 11:10:48 +0000

    wordpress module- updated

--- modules/wordpress-site/manifests/init.pp
+++ modules/wordpress-site/manifests/init.pp
@@ -1,5 +1,6 @@
+#include apache::mod::php
 class wordpress-site (
-  $document_root = '/var/www/html/lvmguide',
+  $document_root = '/var/www/html/mysite',
 ) {
   #####33333333333333333333
   #service {'apache2':
@@ -17,15 +18,21 @@ class wordpress-site (
   # Manage apache, the files for the website will be 
   # managed by the quest tool
   
-  #class { 'apache': 
-  #  default_vhost => false,
-  #}
+  class { 'apache': 
+    mpm_module => 'prefork'
+  }
+  include apache::mod::php
+  include apache::mod::mysql
   #apache::vhost { 'learning.puppetlabs.vm':
   #  port    => $port,
   #  docroot => $document_root,
   #}
+  class { '::mysql::server':
+    root_password    => 'password',
+    override_options => $override_options
+  } 
   class {'wordpress':
-    install_dir => '/var/www/html/mysite',
+    install_dir => $document_root,
   }
   
 }
